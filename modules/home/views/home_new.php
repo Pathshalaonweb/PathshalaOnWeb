8b1421bcf2d4e83374dbe46caf0b9b81e1708c8f
Ishtpreet Singh <500061526@stu.upes.ac.in>
2020-10-03 23:05:58 +0530

    Acadex Counter #2

--- modules/home/views/home_new.php
+++ modules/home/views/home_new.php
@@ -804,17 +804,6 @@ $(document).ready(function() {
         });
 });
 </script>
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5f71a438f0e7167d00145220/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
 </body>
 </html>
 <?php //$this->load->view('bottom_application'); ?>
